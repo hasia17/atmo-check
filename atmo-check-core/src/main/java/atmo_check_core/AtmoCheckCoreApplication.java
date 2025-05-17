976b53d623aa6a92f9bcaf9a1537f0ae60f3b1cf
Agata Wankiewicz <aaggaattaa.wan@gmail.com>
2025-05-17 14:17:54 +0200

    initial project

---
+++ atmo-check-core/src/main/java/atmo_check_core/AtmoCheckCoreApplication.java
@@ -0,0 +1,13 @@
+package atmo_check_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmoCheckCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmoCheckCoreApplication.class, args);
+	}
+
+}
---
+++ atmo-check-core/src/test/java/atmo_check_core/AtmoCheckCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package atmo_check_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmoCheckCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
