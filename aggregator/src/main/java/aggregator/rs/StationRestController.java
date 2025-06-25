3bbca2395902e80bba9b96403608340baac69443
Agata Wankiewicz <aaggaattaa.wan@gmail.com>
2025-06-25 19:33:13 +0200

    Add docker config for aggregator

--- aggregator/src/main/java/aggregator/rs/StationRestController.java
+++ aggregator/src/main/java/aggregator/rs/StationRestController.java
@@ -2,17 +2,27 @@ package aggregator.rs;
 
 import aggregator.api.StationsApi;
 import aggregator.model.Station;
+import aggregator.rs.client.GiosApiClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Controller
 public class StationRestController implements StationsApi {
 
+    private final GiosApiClient giosApiClient;
+
+    public StationRestController(GiosApiClient giosApiClient) {
+        this.giosApiClient = giosApiClient;
+    }
+
     @Override
     public ResponseEntity<List<Station>> getAllStations() {
+        log.info("getAllStations: {} results", giosApiClient.getAllStations().size());
         return ResponseEntity.ok(Collections.emptyList());
     }
 }
