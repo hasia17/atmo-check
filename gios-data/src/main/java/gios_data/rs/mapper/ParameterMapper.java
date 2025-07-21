ea859636a66e4da35f09595f530bb4c5ff8d9b55
Agata Wankiewicz <aaggaattaa.wan@gmail.com>
2025-07-21 19:39:06 +0200

    Fix parametrs mapping

--- gios-data/src/main/java/gios_data/rs/mapper/ParameterMapper.java
+++ gios-data/src/main/java/gios_data/rs/mapper/ParameterMapper.java
@@ -3,18 +3,17 @@ package gios_data.rs.mapper;
 import ext.gios.api.model.GiosSensorLdDTO;
 import gios.data.model.ParameterDTO;
 import gios_data.domain.model.Parameter;
-import org.mapstruct.AfterMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ParameterMapper {
+
     @Mapping(target = "id", source = "idWskaźnika")
     @Mapping(target = "name", source = "wskaźnik")
     @Mapping(target = "description", source = "wskaźnikWzór")
+    @Mapping(target = "unit", ignore = true)
     Parameter map(GiosSensorLdDTO dto);
 
     List<Parameter> map(List<GiosSensorLdDTO> dtos);
@@ -23,10 +22,11 @@ public interface ParameterMapper {
 
     @AfterMapping
     default void setUnit(GiosSensorLdDTO dto, @MappingTarget Parameter parameter) {
-        parameter.setUnit(getUnitForParameterCode(dto.getWskaźnikKod()));
+        parameter.setUnit(determineUnitForParameterCode(dto.getWskaźnikKod()));
     }
 
-    default String getUnitForParameterCode(String code) {
+    @Named("determineUnit")
+    default String determineUnitForParameterCode(String code) {
         if (code == null || code.isEmpty()) return "";
         return switch (code.toUpperCase()) {
             case "PM10", "PM2.5", "SO2", "NO2", "CO", "O3", "C6H6" -> "μg/m³";
