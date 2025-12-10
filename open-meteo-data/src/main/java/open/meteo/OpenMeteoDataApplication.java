b0cd8c2a3359369d67a9ba1f6c89ea73405e1ef5
Agata Wankiewicz <aaggaattaa.wan@gmail.com>
2025-12-10 18:04:41 +0100

    initialize new project open-meteo-data

---
+++ open-meteo-data/src/main/java/open/meteo/OpenMeteoDataApplication.java
@@ -0,0 +1,13 @@
+package open.meteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenMeteoDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenMeteoDataApplication.class, args);
+	}
+
+}
--- open-meteo-data/src/main/java/open/meteo/domain/model/Station.java
+++ open-meteo-data/src/main/java/open/meteo/domain/model/Station.java
@@ -1,18 +1,15 @@
-package gios_data.domain.model;
+package open.meteo.domain.model;
 
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 @Setter
 @Document(collection = "stations")
 public class Station {
+
     @Id
     private String id;
     private String name;
